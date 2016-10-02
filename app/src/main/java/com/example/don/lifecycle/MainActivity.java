aef63c68232b5f3199097e1f9f72369745ecb508
don <protocolkey@gmail.com>
2016-10-02 18:32:35 -0400

    with changes

--- app/src/main/java/com/example/don/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/don/lifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.don.lifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("**LifeCycle**","onCreate");
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("**LifeCycle**","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("**LifeCycle**","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("**LifeCycle**","onRestart);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        Log.e("**LifeCycle**","onDeatachedFromWindow");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("**LifeCycle**","onPause");
     }
 }
